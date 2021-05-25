2d20eae482b3b91c3c13b3a5c808416073d02929
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-05-25 21:40:46 +0200

    add styles and links

--- front-end-libraries/random-quote-machine/src/Components/QuoteButton.js
+++ front-end-libraries/random-quote-machine/src/Components/QuoteButton.js
@@ -6,6 +6,9 @@ function Author({ changeQuote }) {
   };
   return (
     <div id="new-quote" className="new-quote">
+      <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
+        Tweet
+      </a>
       <button onClick={handleQuoteChange}>New Quote</button>
     </div>
   );
