68b1defe5a37fbc22bcf2a8e89ae9ce1c6426daf
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-05-28 19:46:49 +0200

    small fix

--- front-end-libraries/js-calc/src/Components/ResultField.js
+++ front-end-libraries/js-calc/src/Components/ResultField.js
@@ -1,7 +1,13 @@
 import "./ResultField.css";
 
 function ResultField() {
-  return <div className="ResultField">69</div>;
+  return (
+    <div className="ResultField">
+      <p id="currentCalc">69</p>
+      <br />
+      <p id="currentOp">69</p>
+    </div>
+  );
 }
 
 export default ResultField;
