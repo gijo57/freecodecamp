5e0b80b1cd9112be16b2d9769755ea01d50e19b3
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-06-15 15:52:25 +0200

    add more components and refactor

--- front-end-libraries/pomodoro-clock/src/Components/Timer.js
+++ front-end-libraries/pomodoro-clock/src/Components/Timer.js
@@ -1,5 +1,9 @@
 function Timer() {
-  return <div className="Timer"></div>;
+  return (
+    <div className="Timer">
+      <h2 id="session-label">Session Length</h2>
+    </div>
+  );
 }
 
 export default Timer;
