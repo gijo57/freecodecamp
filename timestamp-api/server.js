88d13793de045829d42d24833bfe29ae917c3119
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-27 17:51:46 +0200

    remove html and css

--- timestamp-api/server.js
+++ timestamp-api/server.js
@@ -6,10 +6,6 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
