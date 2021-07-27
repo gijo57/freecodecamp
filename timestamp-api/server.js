ca6b9467faaeddcc06e881d3edbc7c23fa2c1ace
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-27 20:09:43 +0200

    timestamp API for unix and utc timestamps

--- timestamp-api/server.js
+++ timestamp-api/server.js
@@ -1,11 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use(express.static('public'));
+app.get('/api/:date', (req, res) => {
+  const { date } = req.params;
+  let unixStamp, dateStamp;
 
-var listener = app.listen(process.env.PORT, function () {
+  if (Date.parse(date)) {
+    unixStamp = Date.parse(date);
+    dateStamp = new Date(date).toUTCString();
+  } else {
+    unixStamp = date;
+    dateStamp = new Date(Number(date)).toUTCString();
+  }
+
+  res.json({
+    unix: unixStamp,
+    utc: dateStamp
+  });
+});
+
+const listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
