ad2bb341a52990878b68f6fbbd2d58c1908edfb1
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-28 13:36:29 +0200

    create basic request structure and json response

--- back-end-projects/urlshortener/server.js
+++ back-end-projects/urlshortener/server.js
@@ -6,9 +6,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl/', function (req, res) {
+  const url = req.body.url;
+
+  res.json({
+    original_url: url,
+    short_url: 1
+  });
 });
 
 app.listen(port, function () {
