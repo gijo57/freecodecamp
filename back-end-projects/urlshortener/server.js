478c3a2495081f32142ff0ebac8836a8b5af47ad
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-29 13:04:35 +0200

    add mongo and mongoose

--- back-end-projects/urlshortener/server.js
+++ back-end-projects/urlshortener/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -11,10 +12,24 @@ app.use(express.urlencoded());
 
 app.post('/api/shorturl/', function (req, res) {
   const url = req.body.url;
+  let dnsUrl;
+  if (url.includes('https')) {
+    dnsUrl = url.substr(8);
+  } else if (url.includes('http')) {
+    dnsUrl = url.substr(7);
+  } else {
+    dnsUrl = 'Not valid';
+  }
 
-  res.json({
-    original_url: url,
-    short_url: 1
+  dns.lookup(dnsUrl, (err, address, family) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      res.json({
+        original_url: url,
+        short_url: Number(address.substr(0, 3))
+      });
+    }
   });
 });
 
