652212cb18fe67744b8cb289f78ccf51e02c564f
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-30 15:58:54 +0200

    add route to handle and return the upload file metadata

--- back-end-projects/metadata/server.js
+++ back-end-projects/metadata/server.js
@@ -1,14 +1,26 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const fs = require('fs');
+const multer = require('multer');
+const cors = require('cors');
+const upload = multer({ dest: 'uploads/' });
+
 require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file);
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
+  res.json({ name, type, size });
 });
 
 const port = process.env.PORT || 3000;
