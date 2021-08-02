85d5b02ef8b01cc272dd0e898ef5613d32a42136
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-08-02 17:41:36 +0200

    add mongoose and mongo and dev script

--- exercisetracker/server.js
+++ exercisetracker/server.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+const PORT = process.env.PORT || 3000;
 
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
